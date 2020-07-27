4d6ae861d4728799237d2fed6de813f5dd09635d
joao.portugues <joaomatiasportugues@gmail.com>
2020-07-27 17:46:25 +0200

    clean up config

--- codecept.conf.js
+++ codecept.conf.js
@@ -6,7 +6,7 @@ exports.config = {
       browser: 'chrome',
       desiredCapabilities: {
 	    chromeOptions: {
-		  args: [/*"--headless"*/]
+		  args: [/*"--headless",*/"--window-size=1200,1000"]
 	    }
       }
     },
